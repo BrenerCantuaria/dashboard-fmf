d420259b3cb37598924a2f72abd185dc5107188e
Márcio Brener <BrenerCantuariaSantos@gmail.com>
2023-11-03 01:43:57 -0300

    Add link de sair

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,18 +1,32 @@
 import { Link, useLocation } from "react-router-dom";
 import style from "./Header.module.css";
+import { useContext } from "react";
+import { AuthContext } from "../../context/auth/authcontext";
 
 export default function Header() {
+  const auth = useContext(AuthContext);
   const location = useLocation();
-
+  const handleLogOut = async () => {
+    await auth.signout();
+  };
   return (
     <nav className={style.header}>
       <ul>
-        <li className={location.pathname === '/' ? style.active_link : ''}>
+        <li className={location.pathname === "/" ? style.active_link : ""}>
           <Link to="/">DashBoard</Link>
         </li>
-        <li className={location.pathname === '/Clientes' ? style.active_link : ''}>
+        <li
+          className={location.pathname === "/Clientes" ? style.active_link : ""}
+        >
           <Link to="/Clientes">Clientes</Link>
         </li>
+        {auth.user && (
+          <li>
+            <Link to="/Login" onClick={handleLogOut}>
+              Sair
+            </Link>
+          </li>
+        )}
       </ul>
     </nav>
   );
