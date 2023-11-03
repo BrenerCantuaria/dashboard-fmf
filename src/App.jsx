e8c036167f8363d3fdcf515fe96fe6626b4afae3
Márcio Brener <BrenerCantuariaSantos@gmail.com>
2023-11-03 01:45:26 -0300

    Add rota de Login e componente Require

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,24 @@ import "./index.css";
 import Customers from "./pages/customerList";
 import DashBoard from "./pages/dashboard";
 import Home from "./pages/home";
+import Login from "./pages/login";
+import AuthProvider from "./context/auth/authprovider";
+import RequireAuth from "./context/auth/RequireAuth";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />}>
-          <Route path="/" element={<DashBoard />}></Route>
-          <Route path="/Clientes" element={<Customers />}></Route>
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    // Provedor
+    <AuthProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/login" element={<Login />}></Route>
+          <Route path="/" element={<RequireAuth> <Home/> </RequireAuth>}>
+            <Route path="/" element={<DashBoard />}></Route>
+            <Route path="/Clientes" element={<Customers />}></Route>
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </AuthProvider>
   );
 }
 
