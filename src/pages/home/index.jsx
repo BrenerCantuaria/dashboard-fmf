094e48256b6830f20be8671e6d6e4b9b7391284a
Márcio Brener <BrenerCantuariaSantos@gmail.com>
2023-11-03 01:36:52 -0300

    removendo useEffect desnecessário

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,42 +1,14 @@
 import { Outlet } from "react-router-dom";
+import Footer from "../../components/Footer";
 import SideBar from "../../components/Sidebar/SideBar";
 import styles from "./Home.module.css";
-import Loading from "../../components/Load";
-import Footer from "../../components/Footer"
-import { useEffect, useState } from "react";
 
 export default function Home() {
-  const [loading, setLoading] = useState(true);
-  const [token, setToken] = useState(localStorage.getItem("token"));
-
-  useEffect(() => {
-    async function fetchToken() {
-      try {
-        if (!token) {
-          const response = await fetch("URL_AQUI");
-          if (!response.ok) {
-            throw new Error("Erro na requisição");
-          }
-          const data = await response.json();
-          const receivedToken = data.token;
-          setToken(receivedToken);
-          localStorage.setItem("token", receivedToken);
-          setLoading(false); 
-        }
-      } catch (error) {
-        console.error(error);
-        setLoading(false);
-      }
-    }
-
-    fetchToken();
-  }, [token]);
-
   return (
     <main className={styles.container}>
       <SideBar />
-      {loading ? <Loading /> : <Outlet />}
-      <Footer/>
+      <Outlet />
+      <Footer />
     </main>
   );
 }
