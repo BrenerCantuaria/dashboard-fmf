fca07f187e56b5ff018743a2f2f2b2fd80ba946b
Márcio Brener <BrenerCantuariaSantos@gmail.com>
2023-11-03 01:39:56 -0300

    Ajustando código no tab

--- src/pages/dashboard/index.jsx
+++ src/pages/dashboard/index.jsx
@@ -1,25 +1,26 @@
 /* eslint-disable no-unused-vars */
-import style from "./Dashboard.module.css"
-import FlowChart from "../../components/FlowChart"
-import PieChart from "../../components/PieTimeChart"
-import Table from "../../components/Table"
-import Card from "../../components/Card"
-import AppointmentsList from "../../components/AppointmentsList"
-import {Chart} from "chart.js"
+import style from "./Dashboard.module.css";
+import FlowChart from "../../components/FlowChart";
+import PieChart from "../../components/PieTimeChart";
+import Table from "../../components/Table";
+import Card from "../../components/Card";
+import AppointmentsList from "../../components/AppointmentsList";
+import { Chart } from "chart.js";
+
 export default function DashBoard() {
   return (
     <main className={style.container}>
-       <section className={style.grid_item}>
-        <FlowChart/>
-        <PieChart/>
-       </section>
-       <section className={style.card}>
-          <Card title={"Dias de Visita"}/>
-          <AppointmentsList/>
-        </section>
-       <section className={style.grid_table}>
-         <Table/>
-       </section>
+      <section className={style.grid_item}>
+        <FlowChart />
+        <PieChart />
+      </section>
+      <section className={style.card}>
+        <Card title={"Dias de Visita"} />
+        <AppointmentsList />
+      </section>
+      <section className={style.grid_table}>
+        <Table />
+      </section>
     </main>
-  )
-} 
+  );
+}
