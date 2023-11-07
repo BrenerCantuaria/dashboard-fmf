ab2293cb672fb6b76140b9d941ca6f7eacf93b15
Márcio Brener <BrenerCantuariaSantos@gmail.com>
2023-11-07 09:29:03 -0300

    ajustando createContext

--- src/context/auth/authcontext.jsx
+++ src/context/auth/authcontext.jsx
@@ -1,15 +1,16 @@
+/* eslint-disable no-unused-vars */
 import { createContext } from "react";
 
 export const User = {
   id: 0,
   email: "",
-  password: ""
+  password: "",
 };
 
 export const AuthContextType = {
-  user: User,
-  signin: (email,password) => Promise.resolve(true),
-  signout: () => {}
+  user: User || null,
+  signin: (email, password) => Promise.resolve(true),
+  signout: () => {},
 };
 
-export const AuthContext = createContext(AuthContextType);
+export const AuthContext = createContext(null);
