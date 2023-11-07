bf774f8c1aba1651738f1575c9d717080f813725
Márcio Brener <BrenerCantuariaSantos@gmail.com>
2023-11-07 09:30:27 -0300

    Condicional de verificação do usuário e localStorage

--- src/context/auth/RequireAuth.jsx
+++ src/context/auth/RequireAuth.jsx
@@ -1,10 +1,15 @@
+/* eslint-disable react/prop-types */
 import { useContext } from "react";
 import { AuthContext } from "./authcontext";
 import Login from "../../pages/login";
 
 function RequireAuth({ children }) {
+  //
+  // Validar o token antes dessa parte,
+  // 422 no interceptor deve apagar o token e redirecionar para o login
   const auth = useContext(AuthContext);
-  if (!auth.user) {
+  console.log("Passou aqui");
+  if (!auth.user && !localStorage.getItem("user")) {
     return <Login />;
   }
   return children;
