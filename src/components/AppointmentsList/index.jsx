ff70c163c6dbadc74c1ba13e22c31b4c08b1a4b7
Márcio Brener <BrenerCantuariaSantos@gmail.com>
2023-11-07 09:23:13 -0300

    tab e css no Card

--- src/components/AppointmentsList/index.jsx
+++ src/components/AppointmentsList/index.jsx
@@ -1,28 +1,33 @@
 import style from "./AppointmentsList.module.css";
-import agendamentos from "../../db/dbAgendamentos.json"
+import agendamentos from "../../db/dbAgendamentos.json";
 export default function AppointmentsList() {
- 
+  // const [agendamentosState,setAgentamentosState] = useState(null)
+
   return (
     <div className={style.container}>
       <h1> Agendamentos do dia</h1>
       <div className={style.content}>
         <table className={style.table_agendamentos}>
           <thead>
-            <td>Visitante</td>
-            <td>Torre</td>
-            <td>Apartamento</td>
-            <td>Expirado</td>
+            <tr>
+              <th>Visitante</th>
+              <th>Torre</th>
+              <th>Apartamento</th>
+              <th>Expirado</th>
+            </tr>
           </thead>
           <tbody>
-            {agendamentos.map(visitante => {
-              return (  
-              <tr key={visitante.id} >
-                <td>{visitante.nome}</td>
-                <td>{visitante.torre}</td>
-                <td>{visitante.apartamento}</td>
-                <td><span>Expirado</span></td>
-              </tr>
-              )
+            {agendamentos.map((visitante) => {
+              return (
+                <tr key={visitante.id}>
+                  <td>{visitante.nome}</td>
+                  <td>{visitante.torre}</td>
+                  <td>{visitante.apartamento}</td>
+                  <td>
+                    <span className={style.expirado}>Expirado</span>
+                  </td>
+                </tr>
+              );
             })}
           </tbody>
         </table>
