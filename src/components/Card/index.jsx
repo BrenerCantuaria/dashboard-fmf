8ad4a9be77b589e7764ae0bb79029dac2d95f0b8
Márcio Brener <BrenerCantuariaSantos@gmail.com>
2023-11-22 14:49:51 -0300

    add componente card

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,13 +1,42 @@
+import { useEffect, useState } from "react";
+import { useApi } from "../../hooks/useApi";
 import style from "./Card.module.css";
+import dbTeste from "../../db/db.json";
 // eslint-disable-next-line react/prop-types
 export default function Card({ title }) {
-  const data = new Date();
+  const [weeklyVisits, setWeeklyVisits] = useState([]);
+  const api = useApi();
+  const [data,setData] = useState([])
+  
+  useEffect(()=>{
+    const fetchData = async () =>{
+      const response = await api.getRecords()
+      setData(response)
+    }
+    fetchData()
+  },[])
+
+  useEffect(() => {
+   
+
+    const visitasValidas = data.filter(
+      (visita) => visita.data_entrada != ""
+    );
+    const totalVisitasPorDia = visitasValidas.reduce((total, visita) => {
+      console.log("total: ",total)
+      const diaDaSemana = new Date(visita.data_entrada).getDay();
+      total[diaDaSemana]++;
+      return total;
+    }, Array(7).fill(0));
+
+    setWeeklyVisits(totalVisitasPorDia);
+  }, [data]);
   return (
     <div className={style.container}>
       <header>
         <h1>
           {title}
-          <span> {data.toLocaleDateString()}</span>
+          <span> {new Date().getDay()}</span>
         </h1>
       </header>
       <div className={style.content}>
@@ -17,6 +46,7 @@ export default function Card({ title }) {
               <th>Segunda-feira</th>
               <th>Terça-feira</th>
               <th>Quarta-feira</th>
+              <th>Quinta-feira</th>
               <th>Sexta-feira</th>
               <th>Sábado</th>
               <th>Domingo</th>
@@ -24,36 +54,9 @@ export default function Card({ title }) {
           </thead>
           <tbody>
             <tr>
-              <td>12</td>
-              <td>4</td>
-              <td>15</td>
-              <td>15</td>
-              <td>12</td>
-              <td>12</td>
-            </tr>
-            <tr>
-              <td>12</td>
-              <td>4</td>
-              <td>15</td>
-              <td>15</td>
-              <td>12</td>
-              <td>12</td>
-            </tr>
-            <tr>
-              <td>12</td>
-              <td>4</td>
-              <td>15</td>
-              <td>15</td>
-              <td>12</td>
-              <td>12</td>
-            </tr>
-            <tr>
-              <td>12</td>
-              <td>4</td>
-              <td>15</td>
-              <td>15</td>
-              <td>12</td>
-              <td>12</td>
+              {weeklyVisits.map((total, index) => (
+                <td key={index}>{total}</td>
+              ))}
             </tr>
           </tbody>
         </table>
